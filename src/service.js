311648c5849d70a2ab37a42377ce64f8d99206f0
Olga-Lukina <lukina_olga@yahoo.com>
2021-07-21 19:34:06 +0200

    deploy

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.VUE_APP_API_URL,
   // process.env.API_URL
   withCredentials: false,
   headers: {
